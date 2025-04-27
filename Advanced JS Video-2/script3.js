f93bba03373f31ac27a8171558afc87dda1b2d34
Bhumika Dhingra <144475217+Dooms10@users.noreply.github.com>
2025-04-27 18:03:23 +0530

    Add files via upload

---
+++ Advanced JS Video-2/script3.js
@@ -0,0 +1,12 @@
+//3.var let const diff
+
+//var adds itself to the window object
+//let const does not add to window object
+
+//In JS language ismei kuch cheeze aesi hoti hai jo ham use kar skte hai aur vo chheze JS se nhi balki browser se milti hai aur aise sare features jo JS ka part nhi hai fir bhi ise use kar skte hai unhe ham dhund skte hai ek particular object mei jiska naam hai window.
+//window browser ka part hai.window is basically box of features given by browser to use with JS.
+
+
+var a=12;//var adds itself to the window a=12 will be shown.
+//let b=12;//let does not adds itself to the window i.e. if i open console b;12 will not be shown 
+var b=12;//shows up in console after writing window
