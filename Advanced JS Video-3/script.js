de230e27b138a59cc08c83ca4cfa7426fb545ec3
Bhumika Dhingra <144475217+Dooms10@users.noreply.github.com>
2025-04-29 22:37:22 +0530

    Add files via upload

---
+++ Advanced JS Video-3/script.js
@@ -0,0 +1,23 @@
+//HIGHER ORDER FUNCTIONS
+//function abcd(val) {
+
+//}
+//abcd(function(){})
+  
+
+//OR------------
+
+function abcd(){
+    return function(){}
+}
+abcd()
+
+//aisa fn jo accept karle ek aur function ya fir vo return karde ek aur function.
+
+
+//eg hota hai iska foer each function
+
+
+//var arr=[1,2,3,4,5]
+
+//arr.foreach(function(){})
